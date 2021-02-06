5d21998a1bd90f39b4431dfe6975ef3fda57769b
sp-devi <spneuro01@gmail.com>
2021-02-06 23:07:08 +0900

    Add style sass loader

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,18 @@
   <v-container>
     <v-row class="text-center">
       <v-col cols="12">
-
+        <h1 class="display-2 font-weight-bold mb-3">CRUD</h1>
+        <form>
+          <v-row class="text-center">
+            <v-col cols="12">
+              <label>Enter name: </label>
+              <input class="border" type="text" />
+            </v-col>
+            <v-col cols="12">
+              <button class="border">Enter</button>
+            </v-col>
+          </v-row>
+        </form>
       </v-col>
     </v-row>
   </v-container>
@@ -14,8 +25,12 @@ import Vue from "vue";
 export default Vue.extend({
   name: "HelloWorld",
 
-  data: () => ({
-
-  })
+  data: () => ({}),
 });
 </script>
+
+<style lang="scss">
+.border {
+  border-style: solid;
+}
+</style>
