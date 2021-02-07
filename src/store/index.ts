b450ffb8ec54d02ab4be3fb091a6c20b02b7b0f1
sp-devi <spneuro01@gmail.com>
2021-02-07 13:04:57 +0900

    Create axios global instance

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,38 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from "../plugins/axios";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    name: ""
+  },
+  mutations: {
+    MUTATE_NAME(state, name) {
+      state.name = name;
+    }
+  },
+  actions: {
+    async getName({ commit }, name: string) {
+      const res = await axios({
+        url: `/search/${name}`,
+        method: "GET"
+      });
+      return res;
+    },
+    async deleteName(context, name: string) {
+      const res = await axios({
+        url: `/delete/${name}`
+      })
+      return res;
+    },
+    async insertName(context, name: string) {
+      const res = await axios({
+        url: `/add/${name}`
+      });
+      return res;
+    }
+  },
   modules: {}
 });
