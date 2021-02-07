9cf1884c4653be06ccdd6a3d8630875a3ea937cf
sp-devi <spneuro01@gmail.com>
2021-02-07 13:04:24 +0900

    Fix Vue components, add functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,64 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <v-spacer></v-spacer>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld />
-    </v-main>
+    <v-container fill-height>
+      <v-row align="center" justify="center">
+        <v-col cols="12" class="justify-center">
+          <v-row align="center" justify="center">
+            <v-col cols="6" class="justify-center">
+              <v-card width="500px">
+                <v-card-title>
+                  <h1>Name Finder</h1>
+                </v-card-title>
+                <v-card-text>
+                  <v-form>
+                    <v-text-field label="Name" outlined v-model="nameInput" />
+                  </v-form>
+                </v-card-text>
+              </v-card>
+              <v-card-actions>
+                <v-btn color="success" @click="findName(nameInput)">Find</v-btn>
+                <v-btn color="success" @click="addName(nameInput)">Add</v-btn>
+              </v-card-actions>
+            </v-col>
+          </v-row>
+          <Name :names="names"></Name>
+        </v-col>
+      </v-row>
+    </v-container>
   </v-app>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import Name from "./components/Name.vue";
+import { mapActions } from "vuex";
+import axios from "axios";
 
 export default Vue.extend({
   name: "App",
 
   components: {
-    HelloWorld
+    Name,
   },
 
   data: () => ({
-    //
-  })
+    names: [],
+  }),
+
+  methods: {
+    ...mapActions(["getName"]),
+    async findName(name: string) {
+      const responseName = await this.getName(name);
+      const namex = [{ name: responseName.data.name }];
+      this.$data.names = namex;
+    },
+    ...mapActions(["insertName"]),
+    async addName(name: string) {
+      const responseName = await this.insertName(name);
+      alert(responseName);
+      const namex = [{ name: responseName }];
+      this.$data.names = namex;
+    },
+  },
 });
 </script>
---
+++ src/components/Name.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-container>
+    <v-row align="center" justify="center">
+      <v-col cols="6" class="justify-center">
+        <v-card max-width="500" class="mx-auto">
+          <v-toolbar color="green" dark>
+            <v-toolbar-title>Names</v-toolbar-title>
+          </v-toolbar>
+          <v-list subheader two-line>
+            <v-list-item v-for="name in names" :key="name">
+              <v-list-item-avatar>
+                <v-icon class="grey lighten-1" dark>mdi-account</v-icon>
+              </v-list-item-avatar>
+              <v-list-item-content>
+                <v-list-item-title class="pl-2">{{
+                  name.name
+                }}</v-list-item-title>
+              </v-list-item-content>
+              <v-list-item-action>
+                <v-btn icon>
+                  <v-icon color="red" @click="removeName(name.name)"
+                    >mdi-minus</v-icon
+                  >
+                </v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import { mapActions } from "vuex";
+
+export default Vue.extend({
+  name: "Name",
+
+  props: ["names"],
+
+  data: () => ({}),
+
+  methods: {
+    ...mapActions(["deleteName"]),
+    async removeName(name: string) {
+      const responseName = await this.deleteName(name);
+      alert(responseName);
+      const namex = [{ name: responseName }];
+      this.$data.names = namex;
+    },
+  },
+});
+</script>
